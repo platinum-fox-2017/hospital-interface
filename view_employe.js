a0f05d8c405497423bf8ec4d639fec434bf30857
yosaid92 <yohanessahrul92@gmail.com>
2018-02-15 19:39:59 +0700

    release 2

---
+++ view_employe.js
@@ -0,0 +1,13 @@
+class ViewEmployee {
+    static viewList(arr){
+        console.log('Total Employe ' + arr.length)
+    }
+    static viewLogin(notifikasi){
+        console.log(notifikasi)
+    }
+}
+
+
+
+
+module.exports = ViewEmployee
